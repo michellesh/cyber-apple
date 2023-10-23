409b555a73d014b25e21b79d9f97b23aa6acb5d0
Michelle Sharer <michellesharer@gmail.com>
2023-10-23 12:07:52 -0600

    Update test file to use a single LED array

--- cyber-apple-test/cyber-apple-test.ino
+++ cyber-apple-test/cyber-apple-test.ino
@@ -10,34 +10,36 @@
 #define PIN_STEM 3
 
 #define LED_TYPE NEOPIXEL
-#define COLOR_ORDER GRB
-#define BRIGHTNESS 180
+#define BRIGHTNESS 255
 
-#define NUM_LEDS_STRIP 177
+#define NUM_LEDS 177
 #define NUM_LEDS_STEM 31
+#define NUM_STRANDS 6
 
 #define DRIP_STICK_INDEX 65
-#define DRIP_STICK_LENGTH 36
+#define DRIP_STICK_LENGTH 18 // 36
 
-CRGB leds1[NUM_LEDS_STRIP];
-CRGB leds2[NUM_LEDS_STRIP];
-CRGB leds3[NUM_LEDS_STRIP];
-CRGB leds4[NUM_LEDS_STRIP];
-CRGB leds5[NUM_LEDS_STRIP];
-CRGB leds6[NUM_LEDS_STRIP];
-CRGB ledsStem[NUM_LEDS_STRIP];
+CRGB *leds;
+CRGB ledsStem[NUM_LEDS_STEM];
+
+CRGB colors[] = {
+    CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Purple,
+};
 
 void setup() {
   Serial.begin(115200);
   delay(500);
 
-  FastLED.addLeds<LED_TYPE, PIN_1>(leds1, NUM_LEDS_STRIP);
-  FastLED.addLeds<LED_TYPE, PIN_2>(leds2, NUM_LEDS_STRIP);
-  FastLED.addLeds<LED_TYPE, PIN_3>(leds3, NUM_LEDS_STRIP);
-  FastLED.addLeds<LED_TYPE, PIN_4>(leds4, NUM_LEDS_STRIP);
-  FastLED.addLeds<LED_TYPE, PIN_5>(leds5, NUM_LEDS_STRIP);
-  FastLED.addLeds<LED_TYPE, PIN_6>(leds6, NUM_LEDS_STRIP);
-  FastLED.addLeds<LED_TYPE, PIN_STEM>(ledsStem, NUM_LEDS_STRIP);
+  leds = new CRGB[NUM_STRANDS * NUM_LEDS];
+
+  FastLED.addLeds<LED_TYPE, PIN_1>(leds, 0, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, PIN_2>(leds, NUM_LEDS, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, PIN_3>(leds, NUM_LEDS * 2, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, PIN_4>(leds, NUM_LEDS * 3 + 1,
+                                   NUM_LEDS); // strand 4 is shorter by 1 pixel
+  FastLED.addLeds<LED_TYPE, PIN_5>(leds, NUM_LEDS * 4, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, PIN_6>(leds, NUM_LEDS * 5, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, PIN_STEM>(ledsStem, NUM_LEDS_STEM);
 
   FastLED.setBrightness(BRIGHTNESS);
 }
@@ -45,19 +47,11 @@ void setup() {
 void loop() {
   FastLED.clear();
 
-  for (int i = 0; i < NUM_LEDS_STRIP; i++) {
-    CRGB color = CRGB::Red;
-    if (i >= DRIP_STICK_INDEX && i < DRIP_STICK_INDEX + DRIP_STICK_LENGTH) {
-      color = CRGB::Black;
+  for (int i = 0; i < NUM_STRANDS; i++) {
+    for (int j = 0; j < NUM_LEDS; j++) {
+      bool dripStick = j >= DRIP_STICK_INDEX && j < DRIP_STICK_INDEX + DRIP_STICK_LENGTH * 2;
+      leds[i * NUM_LEDS + j] = dripStick ? CRGB::Black : colors[i];
     }
-    leds1[i] = color;
-    leds2[i] = color;
-    leds3[i] = color;
-    if (i > 0) {
-      leds4[i - 1] = color;
-    }
-    leds5[i] = color;
-    leds6[i] = color;
   }
 
   for (int i = 0; i < NUM_LEDS_STEM; i++) {
